3c6bc12e021611590e13c400aeedc665b582a9c3
J. Nick Koston <nick@koston.org>
2023-07-11 14:38:33 -1000

    fix: make MGMTBluetoothCtl aware of down adapters (#38)

--- src/bluetooth_auto_recovery/recover.py
+++ src/bluetooth_auto_recovery/recover.py
@@ -14,6 +14,7 @@ from typing import Any, AsyncIterator, cast
 import async_timeout
 import pyric.net.wireless.rfkill_h as rfkh
 import pyric.utils.rfkill as rfkill
+from bluetooth_adapters import get_adapters_from_hci
 from btsocket import btmgmt_protocol, btmgmt_socket
 from btsocket.btmgmt_socket import AF_BLUETOOTH, BTPROTO_HCI
 from usb_devices import BluetoothDevice, NotAUSBDeviceError
@@ -202,6 +203,31 @@ class MGMTBluetoothCtl:
     async def _find_controller(self) -> None:
         """Find the controller."""
         assert self.protocol is not None  # nosec
+        loop = asyncio.get_running_loop()
+        # Try to get the adapter index from the hci device first
+        # since it can see downed adapters.
+        if adapters_from_hci := await loop.run_in_executor(None, get_adapters_from_hci):
+            _LOGGER.debug("Found adapters from hci: %s", adapters_from_hci)
+            for adapter in adapters_from_hci.values():
+                if adapter["bdaddr"] == self.mac.upper():
+                    self.idx = adapter["dev_id"]
+                    _LOGGER.debug(
+                        "Found adapter %s in hci device as %s", self.mac, self.idx
+                    )
+                    return
+
+            for adapter in adapters_from_hci.values():
+                expected_hci_name = f"hci{self._expected_hci}"
+                if adapter["name"] == expected_hci_name:
+                    self.idx = adapter["dev_id"]
+                    _LOGGER.debug(
+                        "Found adapter %s as hci device %s as %s",
+                        self.mac,
+                        self._expected_hci,
+                        self.idx,
+                    )
+                    return
+
         idxdata = await self.protocol.send("ReadControllerIndexList", None)
         if idxdata.event_frame.status.value != 0x00:  # 0x00 - Success
             _LOGGER.error(
