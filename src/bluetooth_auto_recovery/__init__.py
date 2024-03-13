052dbf93cb0ce19237e6e034da25c5a7df0360d3
J. Nick Koston <nick@koston.org>
2024-03-13 13:31:46 -1000

    fix test

--- src/bluetooth_auto_recovery/__init__.py
+++ src/bluetooth_auto_recovery/__init__.py
@@ -17,12 +17,14 @@ async def recover_adapter(hci: int, mac: str) -> bool:
     the `bluetooth_auto_recovery.recover` module and calls
     its `recover_adapter` function.
     """
-    if not (recover_module := _MODULE_CACHE.get("bluetooth_auto_recovery.recover")):
+    recover_module_name = f"{__package__}.recover"
+
+    if not (recover_module := _MODULE_CACHE.get(recover_module_name)):
         loop = asyncio.get_running_loop()
         recover_module = await loop.run_in_executor(
-            None, importlib.import_module, "bluetooth_auto_recovery.recover"
+            None, importlib.import_module, recover_module_name
         )
-        _MODULE_CACHE["bluetooth_auto_recovery.recover"] = recover_module  # type: ignore
+        _MODULE_CACHE[recover_module_name] = recover_module  # type: ignore
         this_module = sys.modules[__package__]
         this_module.recover_adapter = recover_module.recover_adapter  # type: ignore
 
--- tests/test_init.py
+++ tests/test_init.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 import pytest
 
 import bluetooth_auto_recovery
+from bluetooth_auto_recovery import recover
 
 
 def test_init():
@@ -13,7 +14,11 @@ def test_init():
 @pytest.mark.asyncio
 async def test_recover_adapter():
     """Test the recover_adapter function."""
-    with patch("bluetooth_auto_recovery.recover_adapter") as recover_adapter:
+    assert bluetooth_auto_recovery.recover_adapter is not recover.recover_adapter
+
+    with patch("bluetooth_auto_recovery.recover.recover_adapter") as recover_adapter:
         await bluetooth_auto_recovery.recover_adapter(0, "00:00:00:00:00:00")
 
     assert recover_adapter.called
+    assert bluetooth_auto_recovery.recover_adapter is recover_adapter
+    bluetooth_auto_recovery.recover_adapter = recover.recover_adapter
