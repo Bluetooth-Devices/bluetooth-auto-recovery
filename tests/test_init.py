d9b8eb72a4b9c35691e3e4ff1ebcc47021e85042
J. Nick Koston <nick@koston.org>
2024-03-13 13:19:35 -1000

    feat: only import recovery code the first time the recovery is called
    On the fast majority of system we will never need to reset the adapter
    so we do not want to load all the deps until its needed

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,19 @@
+from unittest.mock import patch
+
+import pytest
+
 import bluetooth_auto_recovery
 
 
 def test_init():
     """Test the init function."""
     assert bluetooth_auto_recovery
+
+
+@pytest.mark.asyncio
+async def test_recover_adapter():
+    """Test the recover_adapter function."""
+    with patch("bluetooth_auto_recovery.recover_adapter") as recover_adapter:
+        await bluetooth_auto_recovery.recover_adapter(0, "00:00:00:00:00:00")
+
+    assert recover_adapter.called
